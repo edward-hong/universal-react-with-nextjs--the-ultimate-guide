b76b0d188565f6909a0f18531118e682d60b8eed
Edward Hong <edward.hong527@gmail.com>
2020-07-03 01:53:53 +1200

    S03L06 - Fetching Next Page of Stories, Adding Pagination with Query Params

--- hacker-next/pages/index.js
+++ hacker-next/pages/index.js
@@ -1,10 +1,11 @@
 import Error from 'next/error'
+import Link from 'next/link'
 import fetch from 'isomorphic-fetch'
 
 import Layout from '../components/Layout'
 import StoryList from '../components/StoryList'
 
-const Index = ({ stories }) => {
+const Index = ({ stories, page }) => {
   if (stories.length === 0) {
     return <Error statusCode={503} />
   }
@@ -14,21 +15,41 @@ const Index = ({ stories }) => {
       description="A Hacker News clone made with Next.js"
     >
       <StoryList stories={stories} />
+      <footer>
+        <Link href={`/?page=${page + 1}`}>
+          <a>Next Page ({page + 1})</a>
+        </Link>
+      </footer>
+
+      <style jsx>{`
+        footer {
+          padding: 1em;
+        }
+
+        footer a {
+          font-weight: bold;
+          color: black;
+          text-decoration: none;
+        }
+      `}</style>
     </Layout>
   )
 }
 
-Index.getInitialProps = async () => {
-  let stories
+Index.getInitialProps = async ({ req, res, query }) => {
+  let stories, page
   try {
-    const response = await fetch('https://node-hnapi.herokuapp.com/news?page=1')
+    page = Number(query.page) || 1
+    const response = await fetch(
+      `https://node-hnapi.herokuapp.com/news?page=${page}`,
+    )
     stories = await response.json()
   } catch (err) {
     console.log(err)
     stories = []
   }
 
-  return { stories }
+  return { stories, page }
 }
 
 export default Index
