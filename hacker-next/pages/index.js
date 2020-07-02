ca111ea21e7012852b25c31c597e1d56d4f7785b
Edward Hong <edward.hong527@gmail.com>
2020-07-03 00:05:33 +1200

    S03L03 Handling Errors with Built-in Error Page and Try / Catch

--- hacker-next/pages/index.js
+++ hacker-next/pages/index.js
@@ -1,3 +1,33 @@
-const Index = () => <div>Hacker Next</div>
+import Error from 'next/error'
+import fetch from 'isomorphic-fetch'
+
+const Index = ({ stories }) => {
+  if (stories.length === 0) {
+    return <Error statusCode={503} />
+  }
+  return (
+    <div>
+      <h1>Hacker Next</h1>
+      <div>
+        {stories.map((story) => (
+          <h2 key={story.id}>{story.title}</h2>
+        ))}
+      </div>
+    </div>
+  )
+}
+
+Index.getInitialProps = async () => {
+  let stories
+  try {
+    const response = await fetch('https://node-hnapi.herokuapp.com/news?page=1')
+    stories = await response.json()
+  } catch (err) {
+    console.log(err)
+    stories = []
+  }
+
+  return { stories }
+}
 
 export default Index
